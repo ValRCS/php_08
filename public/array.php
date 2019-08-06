064217839ccde366db73bf9735e109d18afa4e32
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-06 12:32:16 +0300

    arrayexercise

---
+++ public/array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+<body>
+    <h1>Array Playground</h1>
+<?php
+    //rewrite this using foreach syntax so it works
+    $b = [1,3,5,53,5,7];
+    $b[10]=55;
+    for ($i = 0; $i < count($b); $i++) {
+        echo "\$b[$i] is".$b[$i];
+        echo "<br>";
+    }
+
+?>
+</body>
+</html>
