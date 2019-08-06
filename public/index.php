00d30a2b80e8d684b7681665a4d01774231aaf0a
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-06 12:29:44 +0300

    Array operations and loops

--- public/index.php
+++ public/index.php
@@ -70,6 +70,84 @@
         myTest();
         myTest();
         myTest();
+
+        $a12 = 120000000000;
+        const MY_PI = 3.14;
+        var_dump($a12);
+
+        echo "<hr>";
+        echo "Largest int".PHP_INT_MAX;
+        echo "<hr>";
+        echo PHP_INT_MIN;
+        echo "<hr>";
+        echo MY_PI;
+        echo "<hr>";
+        
+        echo "My variable a: {$a12} and ".MY_PI." more stuff {$myString}";
+        echo "<hr>";
+        $truth = $a > $a12;
+        echo (int)$truth;
+        var_dump($truth);
+        echo "<hr>";
+        $cars = array("Volvo","BMW","Toyota");
+        var_dump($cars);
+        echo "<hr>";
+        echo $cars[1];
+        echo "<hr>";
+        $cars['myfavorite'] = "Zapins";
+        var_dump($cars);
+        echo "<hr>";
+        echo $cars["myfavorite"];
+        $cars[100]="Volga";
+        //with empty brackets we get the next index after the largest
+        $cars[]="Mersis";
+
+        array_push($cars, "Volvo", "VW", $a12);
+        $a13 = array_pop($cars);
+        echo $a13;
+        var_dump($cars);
+        echo "<hr>";
+
+        $mytable = "<table style='border: 2px solid;'>";
+        $mytable .= "<tr><td>KEY</td><td>VALUE</td></tr>";
+        foreach($cars as $key => $value) {
+            $mytable .= "<tr id='$key'>";
+            $mytable .= "<td>$key</td>";
+            $mytable .= "<td>$value</td>";
+            $mytable .= "</tr>";
+        }
+        $mytable .= "</table>";
+        echo $mytable;
+
+        echo "We have this many cars ".count($cars);
+        echo "<hr>";
+
+        foreach($cars as $value) {
+            echo "Just the Value=" . $value;
+            echo "<br>";
+        }
+        $a=array("A","Cat","Dog","A","Dog");
+        $b = [1,3,5,53,5,7];
+        $b[10]=55;
+        for ($i = 0; $i < count($b); $i++) {
+            echo "\$b[$i] is".$b[$i];
+            echo "<br>";
+        }
+        $c = [
+            1 => "Foo",
+            5 => "Bar", 
+            "Fizz" => "Buzz"
+        ];
+        $c[]="Fizzbuzz";
+        var_dump($c);
+        echo "<hr>";
+        echo $c[6];
+        echo "<hr>";
+        var_dump($b);
+        $counter = array_count_values($a);
+        print_r($counter);
+        echo "<hr>";
+        echo $counter['A'];
     ?>
 </body>
 </html>
