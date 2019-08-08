80271b109c827434a3e0ad8af7daed0ca3481970
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-08 12:11:19 +0300

    added  database connection and row retrieval

--- public/db.php
+++ public/db.php
@@ -11,6 +11,19 @@
     echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
     echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
   
+    $sql = "SELECT * FROM tracks";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["album"]. "<br>";
+        }
+        echo "<hr>";
+    } else {
+        echo "0 results";
+    }
+
 
     mysqli_close($conn);
 
