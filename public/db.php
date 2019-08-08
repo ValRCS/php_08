edbabdbb5c6851902c135a4d192905ab70ea5761
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-08 11:18:26 +0300

    added db files

---
+++ public/db.php
@@ -0,0 +1,18 @@
+<?php
+    $conn = mysqli_connect("127.0.0.1","root", "", "music08");
+
+    if (!$conn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+    
+
+    mysqli_close($conn);
+
+
