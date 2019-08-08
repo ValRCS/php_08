e959c49a46b690e8569dbc1ba365963250d269f9
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-08 12:31:44 +0300

    added support for config

--- public/dball.php
+++ public/dball.php
@@ -1,5 +1,6 @@
 <?php
-    $conn = mysqli_connect("127.0.0.1","root", "", "music08");
+    require_once("../config/config.php");
+    $conn = mysqli_connect(SERVER, USER, PW, DB);
 
     if (!$conn) {
         echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -10,22 +11,25 @@
     
     echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
     echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
-  
+    echo "<br>";
+
     $sql = "SELECT * FROM tracks";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
+    $mydata = $result->fetch_all(MYSQLI_ASSOC);
+    mysqli_close($conn);
 
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["album"]. "<br>";
-        }
+    foreach ($mydata as $key => $row) {
+        echo "ROW:$key :";
+        // var_dump($row);
+        echo $row['name'];
         echo "<hr>";
-    } else {
-        echo "0 results";
     }
 
 
-    mysqli_close($conn);
+  
+
+
+
 
 
