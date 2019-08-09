1a425f7faf0aa533a677891ed072ab1cf75c7856
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-09 12:09:20 +0300

    Added Filtering

--- public/show.php
+++ public/show.php
@@ -4,5 +4,8 @@
     // $myres = getRows("SELECT * FROM tracks");
     // printTable($myres, "mytablestyle");
     //printHTMLHeader
+    echo $_GET["name"];
+    echo $_GET["artist"];
+    echo "<hr>";
     printTable(getRows("SELECT * FROM tracks"), "mytablestyle");
     require_once("../src/foot.php");
---
+++ public/submit.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("../src/utilities.php");
+    require_once("../src/head.php");
+?>
+    <form>
+        <input name="title">
+        <input name="artist" placeholder="Enter artist">
+        <input name="maxsongs" type="number">
+        <button type="submit">SUBMIT</button>
+    </form>
+   <a href="submit.php?maxsongs=10">Max 10</a>
+   <a href="submit.php?order=ASC">Order By Ascending Title</a>
+<?php
+    $qry = "SELECT * FROM tracks"; 
+    if (isset($_GET["maxsongs"])) {
+        $qry .= " LIMIT ".$_GET["maxsongs"]; //FIXME add sanitization!!!
+    } 
+    if (isset($_GET["order"])) {
+        $qry .= " ORDER BY name ".$_GET["order"]; //FIXME add sanitization!!!
+    } 
+
+    printTable(getRows($qry), "mytablestyle");
+
+
+    require_once("../src/foot.php");
