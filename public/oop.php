e6c8adbf63e8ab45b84e6fee97605df38fa5dfff
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-14 11:36:18 +0300

    Constructors and Deconstructors

--- public/oop.php
+++ public/oop.php
@@ -1,9 +1,20 @@
 <?php
     //here we create a new class instance based on our DB declaration
     require_once("../src/DB.php");
-    $mydb = new DB();
-    $mydb2 = new DB();
+    $mydb = new DB(5);
+    $mydb2 = new DB("bobobo");
     echo "my first db ".$mydb->name."<hr>";
     echo "my second db ".$mydb2->name."<hr>";
+    //generally not a good practice to change object properties directly
     $mydb2->name = "This is my second db really!";
     echo "my second db ".$mydb2->name."<hr>";
+    $mydb->printName();
+    $mydb2->printName();
+    // $mydb2->printAnything("kartupelis");
+    echo "My secret is".$mydb->getSecret()."<hr>";
+    echo "My secret is".$mydb2->getSecret()."<hr>";
+
+    $mydb2->setSecret("bumbieris");
+
+    echo "My secret is".$mydb2->getSecret()."<hr>";
+
--- src/DB.php
+++ src/DB.php
@@ -2,4 +2,42 @@
     class DB {
         //code goes here
         public $name = "My db";
+        
+        //for private use only inside class instance
+        private $secret = "arbuzs";
+        private $foo = "Bar";
+        private $fullname = "";
+
+        //constructor gets called each time we create a new object from our class
+        public function __construct($text) {
+            $this->fullname = $this->name . $text;
+            echo "Created new DB! $this->fullname<hr>";
+        }
+
+        public function __destruct() {
+            echo "Destroyed ".$this->fullname;
+        }
+
+        //getter
+        public function getSecret() {
+            //we can format here
+            return $this->secret;
+        }
+
+        //setter
+        public function setSecret($val) {
+            //we can perform validation and sanitization here if needed
+            $this->secret = $val;
+        }
+
+        public function printName() {
+            $this->printAnything("My DB is".$this->name);
+        }
+
+        //utility function for work inside class
+        private function printAnything($text) {
+            echo "Printing some text:".$text."<hr>";
+        }
+
+        
     }
