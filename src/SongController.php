f94c0694d708696fa0139489c7f1a543574947e2
Valdis Saules <valdis.s.coding@gmail.com>
2019-08-15 12:41:51 +0300

    refactored SongController

--- src/SongController.php
+++ src/SongController.php
@@ -1,12 +1,29 @@
 <?php
     require_once("Controller.php");
     class SongController extends Controller {
+        private function processGet() {
+            $username = "";
+            $uid = 0;
+            if (isset($_SESSION['username'])) {
+                $username = $_SESSION['username'];
+            }
+            if (isset($_SESSION['id'])) {
+                $uid = $_SESSION['id'];
+            }
+            //TODO check $_GET for arguments
+            $mysongs = $this->model->getTracks($uid);
+            $this->view->render($mysongs, $username);  
+        }
+
+        private function processPost() {
+            //process Post here
+        }
+        
         public function process() {
             if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-                //TODO check $_GET for arguments
-                $mysongs = $this->model->getTracks(5);
-                $this->view->render($mysongs, "Valdis");
+                $this->processGet();
+            } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $this->processPost();
             }
-
         }
     }
